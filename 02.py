45c7821c88c7e03f8a99b721c03ebb74ecb2c0b6
Matthew White <Matthew.White02@gmail.com>
2022-12-02 21:48:50 +0000

    Update solution to solve for part 2

--- 02.py
+++ 02.py
@@ -9,21 +9,25 @@ with open("input-02.txt", "r") as file:
 # # Example answer = 15
 
 shapeScores = {'A': 1, 'B': 2, 'C': 3,
-               'X': 1, 'Y': 2, 'Z': 3}
+               'X': 1, 'Y': 2, 'Z': 3} # relevant to part 1 only
+
+outcomeScores = {'X': 0, 'Y': 3, 'Z': 6}
+
+def determineShape(line):
+    (u, i) = line.split()
+    shape = shapeScores[u]
+    if i == 'X': shape -= 1
+    elif i == 'Z': shape += 1
+    shape %= 3
+    return ['Z', 'X', 'Y'][shape]
 
 def shapeScore(line):
-    (u, i) = line.split()
-    return shapeScores[i]
+    shape = determineShape(line)
+    return shapeScores[shape]
 
 def outcomeScore(line):
     (u, i) = line.split()
-    outcome = shapeScores[u] - shapeScores[i]
-    if outcome == 0: # result = draw
-        return 3
-    elif outcome % 3 == 1: # result = loss
-        return 0
-    else: # 
-        return 6
+    return outcomeScores[i]
 
 def roundScore(line):
     return shapeScore(line) + outcomeScore(line)
