3950a485063c3abe254911c0e2aedcd369ea069f
Matthew White <Matthew.White02@gmail.com>
2022-12-02 21:49:10 +0000

    Run linting on script

--- 02.py
+++ 02.py
@@ -9,28 +9,35 @@ with open("input-02.txt", "r") as file:
 # # Example answer = 15
 
 shapeScores = {'A': 1, 'B': 2, 'C': 3,
-               'X': 1, 'Y': 2, 'Z': 3} # relevant to part 1 only
+               'X': 1, 'Y': 2, 'Z': 3}  # relevant to part 1 only
 
 outcomeScores = {'X': 0, 'Y': 3, 'Z': 6}
+
 
 def determineShape(line):
     (u, i) = line.split()
     shape = shapeScores[u]
-    if i == 'X': shape -= 1
-    elif i == 'Z': shape += 1
+    if i == 'X':
+        shape -= 1
+    elif i == 'Z':
+        shape += 1
     shape %= 3
     return ['Z', 'X', 'Y'][shape]
+
 
 def shapeScore(line):
     shape = determineShape(line)
     return shapeScores[shape]
 
+
 def outcomeScore(line):
     (u, i) = line.split()
     return outcomeScores[i]
 
+
 def roundScore(line):
     return shapeScore(line) + outcomeScore(line)
+
 
 totalScore = 0
 for line in lines:
