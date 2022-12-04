086a66ebd341588cbd65224277e7091ce57c802d
Matthew White <Matthew.White02@gmail.com>
2022-12-04 21:09:41 +0000

    Add solution for day 4

---
+++ 04.py
@@ -0,0 +1,36 @@
+with open("input-04.txt", "r") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+# # Example input:
+# lines = """2-4,6-8
+# 2-3,4-5
+# 5-7,7-9
+# 2-8,3-7
+# 6-6,4-6
+# 2-6,4-8
+# """.splitlines()
+
+
+# Example answer = 2
+
+
+def parseLine(line):
+    sections = [sections.split("-") for sections in line.split(",")]
+    sections = [list(range(int(s[0]), int(s[1])+1)) for s in sections]
+    return sections[0], sections[1]
+
+
+def fullyContains(a1: list, a2: list):
+    overlap = set(a1).intersection(set(a2))
+    if len(overlap) == len(a1) or len(overlap) == len(a2):
+        return True
+    return False
+
+
+fullOverlaps = 0
+for line in lines:
+    (area1, area2) = parseLine(line)
+    if fullyContains(area1, area2):
+        fullOverlaps += 1
+
+print(fullOverlaps)
