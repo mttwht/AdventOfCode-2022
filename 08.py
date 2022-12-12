54b7a7a881a069ad6f9058741cff9b8dec07c823
Matthew White <Matthew.White02@gmail.com>
2022-12-12 21:32:59 +0000

    Update to solve part 2

--- 08.py
+++ 08.py
@@ -8,30 +8,36 @@ with open("input-08.txt", "r") as file:
 # 33549
 # 35390
 # """.splitlines()
-# # Example answer = 21
+# # Example answer = 8
 
 
 tree_map = [[int(tree) for tree in line] for line in lines]
 
-visible_trees = (len(tree_map) - 1) * 4
+score_map = []
 for row in range(1, len(tree_map) - 1):
+    row_list = []
     for col in range(1, len(tree_map[row]) - 1):
         height = tree_map[row][col]
-        if height > max([tree_map[x][col] for x in range(row)]):
-            # visible from N
-            visible_trees += 1
-            continue
-        elif height > max([tree_map[row][x] for x in range(col + 1, len(tree_map[row]))]):
-            # visible from E
-            visible_trees += 1
-            continue
-        elif height > max([tree_map[x][col] for x in range(row + 1, len(tree_map))]):
-            # visible from S
-            visible_trees += 1
-            continue
-        elif height > max([tree_map[row][x] for x in range(col)]):
-            # visible from W
-            visible_trees += 1
-            continue
 
-print(visible_trees)
+        n, e, s, w = 0, 0, 0, 0
+        for h in [tree_map[x][col] for x in range(row-1, -1, -1)]:
+            # visible to N
+            n += 1
+            if h >= height: break
+        for h in [tree_map[row][x] for x in range(col+1, len(tree_map[row]))]:
+            # visible to E
+            e += 1
+            if h >= height: break
+        for h in [tree_map[x][col] for x in range(row+1, len(tree_map))]:
+            # visible to S
+            s += 1
+            if h >= height: break
+        for h in [tree_map[row][x] for x in range(col-1, -1, -1)]:
+            # visible to W
+            w += 1
+            if h >= height: break
+        row_list.append(n * e * s * w)
+    score_map.append(row_list)
+
+max_score = max([max(row) for row in score_map])
+print(max_score)
