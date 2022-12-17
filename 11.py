a13ee64414221712bd2108623e7db2ba2e321fa7
Matthew White <Matthew.White02@gmail.com>
2022-12-17 22:11:56 +0000

    Add inline docs and format file

--- 11.py
+++ 11.py
@@ -1,6 +1,7 @@
 with open("input-11.txt", "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
+
 # # Example input:
 # lines = """Monkey 0:
 #   Starting items: 79, 98
@@ -34,6 +35,8 @@ with open("input-11.txt", "r") as file:
 
 
 def apply_relief(item: int) -> int:
+    # applies a value of relief for when an item is not damaged
+
     return item % worry_limit
 
 
@@ -52,6 +55,8 @@ class Monkey:
         return ", ".join([str(item) for item in self.items])
 
     def inspect(self, item: int) -> int:
+        # determine the new value of an item after inspecting it
+
         if self.op[0] == '+':
             return item + int(self.op[1])
         elif self.op[0] == '*':
@@ -60,6 +65,8 @@ class Monkey:
             return item * int(self.op[1])
 
     def inspect_item(self) -> int:
+        # inspect the next held item
+
         if self.items:
             self.inspections += 1
             item = self.items.pop(0)
@@ -68,32 +75,45 @@ class Monkey:
             return item
 
     def who_to_throw_to(self, item: int) -> int:
+        # find out who this monkey will throw the item to
+
         if item % self.test_val == 0:
             return self.true
         return self.false
 
 
 def parse_monkeys(lines: list[str]) -> list[Monkey]:
+    # parse monkeys from input data
+
     monkeys = list()
     for i in range(0, len(lines), 7):
-        monkeys.append(Monkey(lines[i:i+6]))
+        monkeys.append(Monkey(lines[i:i + 6]))
     return monkeys
 
 
 def print_monkey(index: int, monkey: Monkey) -> None:
+    # print the state of a monkey
+
     print("Monkey", index, ":", monkey)
 
 
 def print_monkeys(monkeys: list[Monkey]) -> None:
+    # print the state of all monkeys
+
     for monkey in monkeys:
         print_monkey(monkeys.index(monkey), monkey)
 
 
-def print_monkey_business_levels(monkeys: list[Monkey]):
+def print_monkey_inspection_counts(monkeys: list[Monkey]):
+    # print the inspection counts for all monkeys
+
     for m in range(len(monkeys)):
         print("Monkey", m, "inspected items", monkeys[m].inspections, "times.")
 
+
 def monkey_business_level(monkeys: list[Monkey]) -> int:
+    # calculate the total level of monkey business
+
     total_inspections = [m.inspections for m in monkeys]
     cheeky_monkeys = sorted(total_inspections)[-2:]
     return cheeky_monkeys[0] * cheeky_monkeys[1]
@@ -112,8 +132,8 @@ for i in range(10000):
             item = monkey.inspect_item()
             m2 = monkey.who_to_throw_to(item)
             monkeys[m2].items.append(item)
-    print("== After round", i+1, "==")
-    print_monkey_business_levels(monkeys)
+    print("== After round", i + 1, "==")
+    print_monkey_inspection_counts(monkeys)
     print()
 
 print(monkey_business_level(monkeys))
