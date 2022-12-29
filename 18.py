25af98c61beae4b2e77ad2cacfd4baf28704ea15
Matthew White <Matthew.White02@gmail.com>
2022-12-29 18:15:01 +0100

    Update to solve for part 2

--- 18.py
+++ 18.py
@@ -17,7 +17,7 @@ with open("input-18.txt", "r") as file:
 # 2,1,5
 # 2,3,5
 # """.splitlines()][1:]
-# # Example answer = 64
+# # Example answer = 58
 
 
 def parse_input(lines):
@@ -26,14 +26,26 @@ def parse_input(lines):
 
 def count_exposed_sides(cubes: list[tuple[int]]):
     exposed_sides = 0
-    for cube in cubes:
-        for d in range(len(cube)):  # d = dimension
+    open_air = [(0,0,0)]
+    i = 0
+    max_xyz, min_xyz = [], []
+    for d in range(3):
+        max_xyz.append(max([cube[d] for cube in cubes]))
+        min_xyz.append(min([cube[d] for cube in cubes]))
+
+    while i < len(open_air):
+        block = open_air[i]
+        for d in range(len(block)):  # d = dimension
             for v in [1, -1]:
-                temp_cube = list(cube)
+                temp_cube = list(block)
                 temp_cube[d] += v
                 temp_cube = tuple(temp_cube)
-                if not cubes.count(temp_cube):
-                    exposed_sides += 1
+                if min_xyz[d] - 1 <= temp_cube[d] <= max_xyz[d] + 1:
+                    if cubes.count(temp_cube):
+                        exposed_sides += 1
+                    elif not open_air.count(temp_cube):
+                        open_air.append(temp_cube)
+        i += 1
     return exposed_sides
 
 
